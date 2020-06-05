05954a375ba09d615dd0b987fd33317eb4f09f7d
shubhamkumar96 <shubhamkumarranu@gmail.com>
2020-06-05 23:33:44 +0530

    Adding Logic for differentiating File or Folder

--- index.js
+++ index.js
@@ -2,9 +2,21 @@
 
 const fs = require("fs");
 
-fs.readdir(process.cwd(), (err, filenames) => {
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) {
     console.log(err);
   }
-  console.log(filenames);
+
+  const statPromises = filenames.map((filename) => {
+    return lstat(filename);
+  });
+
+  const allStats = await Promise.all(statPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+    console.log(filenames[index], stats.isFile());
+  }
 });
