e7d164fb969f722c4ffd5a35b52328c311440099
shubhamkumar96 <shubhamkumarranu@gmail.com>
2020-06-07 23:46:16 +0530

    Accepting Command-Line Arguments

--- index.js
+++ index.js
@@ -2,15 +2,20 @@
 
 const fs = require("fs");
 const chalk = require("chalk");
+const path = require("path");
+
 const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+//  Accepting Command Line Arguments.
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
   const statPromises = filenames.map((filename) => {
-    return lstat(filename);
+    return lstat(path.join(targetDir, filename));
   });
 
   const allStats = await Promise.all(statPromises);
